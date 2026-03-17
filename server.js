d4134e44010e32fd546d4dabed8eaeb5b98b4acc
zureshm <zuresh.m@gmail.com>
2026-03-17 23:24:44 +0530

    sur strategy implimented

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const http = require("http");
-const { evaluateEMACross } = require("./strategy/emaCrossStrategy");
+const { evaluateEMACross } = require("./strategy/newStrategy");
 
 const app = express();
 app.use(express.json());
